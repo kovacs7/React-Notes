723de9993501b13540d4656fe12a519d6a0dcb25
kovacs7 <asishkumarbehera.official@gmail.com>
2024-03-22 12:45:06 +0530

    functional components examples in Content.jsx, useState hooks in Content.jsx

---
+++ lesson-1/src/components/Content.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+const Content = () => {
+    
+    // useState hooks
+    const [name, setName] = useState("realName--takeshiKovacs") // array destructuring
+    const [count, setCount] = useState(0) 
+
+    const randomNum = ()=>{
+        return Math.floor(((Math.random())*10 + 1))
+        // functional components example
+    }
+
+    const nameChange = ()=>{
+        setName("userName--kovacs7")
+    }
+
+    const nameChange2 = ()=>{
+        setName("playerMode--Active")
+    }
+    
+    const counterHandle = ()=>{
+        setCount(count+1)
+        setCount(count+1)
+    }
+    
+  return (
+    <main>
+        <button onClick={counterHandle}>Counter {count}</button>
+        <br/>
+        <p>{name}</p>
+        <button onClick={nameChange} onDoubleClick={nameChange2}>Name Change Cick</button> 
+        {/* react fast re-rendering without refreshing*/}
+        <p>Returning a random number -- {randomNum()}</p>
+    </main>
+  )
+}
+
+export default Content
---
+++ lesson-1/src/components/Header.jsx
@@ -0,0 +1,36 @@
+
+
+const Header = () => {
+
+    const headerStyle = {
+        backgroundColor: "royalblue",
+        color: "whitesmoke",
+    }
+   // Inline style takes object as its argument
+
+    const handleClick = () => {
+       console.log("you clicked it 1")
+    }
+
+    const handleClick2 = () => {
+       console.log("you clicked it 2")
+    }
+
+    const handleClick3 = (e) => {
+       console.log(e.target.append(" -- Hey you actually clicked me, how dare you!!"))
+    }
+
+  return (
+    <header style={headerStyle}>
+        <h1>This is my header component</h1>
+        <p>This is my para component</p>
+        <button onClick={handleClick}>Click Me1</button>
+        {/* don't use oprators inside onClick fn, it will exceute onclick fn immediately */}
+        <button onDoubleClick={handleClick2}>Click Me2</button>
+        <button onClick={(e) =>{handleClick3(e)}}>Click Me3</button>
+        {/* in the above case, you have to use () in fn otherwise its not going to work. You can even pass param in this way if you want to */}
+    </header>
+  )
+}
+
+export default Header
